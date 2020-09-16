13b09a35e880bf4c107e8e518cc5081358ea6f88
Michael Li <mli25782@gmail.com>
2020-09-15 22:18:08 -0400

    router links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 import Home from './components/Home/Home';
 
@@ -6,20 +12,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Nav className="justify-content-center" activeKey="/home">
-        <Nav.Item>
-          <Nav.Link href="#">General Opioid Information and Statistics</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#">Donate and Join CORA</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="#">Research Portion</Nav.Link>
-        </Nav.Item>
-      </Nav>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav className="justify-content-center" activeKey="/home">
+          <Nav.Item>
+            <Nav.Link href="/general-info">General Opioid Information and Statistics</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/donate">Donate and Join CORA</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/research">Research Portion</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Switch>
+          <Route path="/general-info">
+            <div>info</div>
+          </Route>
+          <Route path="/donate">
+            <div>donate</div>
+          </Route>
+          <Route path="/research">
+            <div>research</div>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
