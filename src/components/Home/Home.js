6a6e6a6294dbbe541465278c3bc68a39804cb61d
Michael Li <mli25782@gmail.com>
2020-09-20 19:47:16 -0400

    searchbar-autosuggest

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,26 +1,92 @@
 import React from 'react';
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from 'react-places-autocomplete';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 import './Home.css';
 
-function Home() {
-  return (
-    <div className="Home">
-      <Card className="container">
-        <Card.Body>
-          <h1>Find a treatment facility near you</h1>
-        </Card.Body>
-        <Form id="searchForm">
-          <Form.Group controlId="searchTerm">
-            <Form.Control size="lg" type="text" placeholder="City or zip code" />
-          </Form.Group>
-          <Button variant="primary" type="submit">Search</Button>
-        </Form>
-      </Card>
-    </div>
-  )
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: '',
+    }
+  }
+
+  handleSearchChange = address => {
+    this.setState({ address });
+  };
+
+  handleSelect = address => {
+    this.setState({ address });
+    geocodeByAddress(address)
+      .then(res => getLatLng(res[0]))
+      .then(latLng => console.log('Success', latLng))
+      .catch(e => console.error(e));
+  };
+
+  renderAutoCompleteSearch = () => {
+    return(
+      <PlacesAutocomplete
+        value={this.state.address}
+        onChange={this.handleSearchChange}
+        onSelect={this.handleSelect}
+      >
+        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+          <div>
+            <input
+              {...getInputProps({
+                placeholder: 'Search Places ...',
+                className: 'location-search-input',
+              })}
+            />
+            <div className="autocomplete-dropdown-container">
+              {loading && <div>Loading...</div>}
+              {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                    key={suggestion.description}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        )}
+      </PlacesAutocomplete>
+    );
+  }
+
+  render(){
+    return (
+      <div className="Home">
+        <Card className="container" id="card-container">
+          <Card.Body>
+            <h3>Millions of Americans Have an Opiod-Use Disorder and Even More Misuse Them. <br/> Help is available.</h3>
+          </Card.Body>
+          <Form id="searchForm">
+            {this.renderAutoCompleteSearch()}
+          </Form>
+        </Card>
+      </div>
+    )
+  }
 }
 
 export default Home;
